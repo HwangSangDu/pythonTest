a52d97a9bc0d61318217761712741787d55fc718
ADAM HWANG <ghkdtkden93@naver.com>
2019-02-13 00:58:15 +0900

    Update class, module python

---
+++ module.py
@@ -0,0 +1,17 @@
+# mod1.py 
+def add(a, b): 
+    return a+b
+
+def sub(a, b): 
+    return a-b
+
+# print(add(1, 4))
+# print(sub(4, 2))
+
+
+# import 할 때 실행되지 않는다.
+if __name__ == "__main__":
+    print(add(1, 4))
+    print(sub(4, 2))
+
+
---
+++ package.py
@@ -0,0 +1,12 @@
+""" from game.sound import *
+echo.echo_test() """
+
+
+# render.py
+from game.sound.echo import echo_test
+
+def render_test():
+    print ("render")
+    echo_test()
+
+render_test()
